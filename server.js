cbfa99475e7d37923ee75942afe8d080e8e6e29e
omsahoo2911 <omsahoo2911@gmail.com>
2022-06-23 01:23:53 -0400

    Test4

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use('/', indexRouter)
 app.use('/restaurants', restaurantRouter)
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3502, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
